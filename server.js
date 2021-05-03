084b0117739c76c51998ddbb5dc812362fbefd84
Solidshots <seanbrent5@gmail.com>
2021-05-03 18:32:55 -0400

    established database connection

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 //Set up the appropriate port for the server and instantiate a server object
 const PORT = process.env.PORT || 3001;
@@ -8,10 +9,24 @@ const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+//Connect to our database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //MySQL username - default 'root'
+        user: 'root',
+        //MySQL password
+        password: 'Pluto1486!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
+db.query('SELECT * FROM candidates', (err, rows)=>{
+    console.log(rows);
+});
 
-
-//Catch all route that must be used last to return an error
+//Catch all route that must be used last to return a NOT FOUND error
 app.use((req, res)=>{
     res.status(404).end();
 })
